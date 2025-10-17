4d1ca6388c84629e840ebe29e0ae99ba802adb12
davfuerod <davidfr0303@gmail.com>
2025-10-17 16:41:15 +0200

    feat: add jest config file

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/server/services/**/*.ts',
+    '!**/node_modules/**',
+    '!**/__tests__/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};

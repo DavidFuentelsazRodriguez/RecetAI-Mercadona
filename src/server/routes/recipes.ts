5dc15612af12c08d441a3d5a209e3900e755fe40
davfuerod <davidfr0303@gmail.com>
2025-11-14 10:32:05 +0100

    refactor: remove unused endpoint

--- src/server/routes/recipes.ts
+++ src/server/routes/recipes.ts
@@ -4,6 +4,5 @@ import * as recipeController from '../controllers/recipeController';
 const router = Router();
 
 router.post('/recipes/generate', recipeController.generateRecipe);
-router.get('/recipes/default-params', recipeController.getDefaultRecipeParams);
 
 export default router;

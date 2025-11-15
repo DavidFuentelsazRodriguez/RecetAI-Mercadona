eb2dec60e609c9596d48620cf5061ba0cde588d1
davfuerod <davidfr0303@gmail.com>
2025-11-15 10:26:37 +0100

    feat: add winston configuration file

---
+++ src/server/config/logger.ts
@@ -0,0 +1,26 @@
+import winston, {Logform} from 'winston';
+
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
+const textFormat = combine(
+  colorize({ all: true }),
+  timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  align(),
+  printf((info: Logform.TransformableInfo) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+);
+
+const jsonFormat = combine(
+  timestamp(),
+  winston.format.json()
+);
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  format: process.env.NODE_ENV === 'development' ? textFormat : jsonFormat,
+  transports: [
+    new winston.transports.Console(),
+  ],
+  exitOnError: false,
+});
+
+export default logger;

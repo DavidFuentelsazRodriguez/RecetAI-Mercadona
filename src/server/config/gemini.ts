b496419f7ce0aee3f97c2141b76999d236b3933c
davfuerod <davidfr0303@gmail.com>
2025-11-20 09:17:44 +0100

    chore: export gemini instance

--- src/server/config/gemini.ts
+++ src/server/config/gemini.ts
@@ -7,10 +7,11 @@ if (!process.env.GOOGLE_API_KEY) {
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 export const gemini = {
+  genAI,
   recipeModel: genAI.getGenerativeModel({
     model: 'gemini-2.0-flash', 
     generationConfig: {
-      temperature: 0.2,         
+      temperature: 0.7,         
       topP: 0.8,                
       topK: 40,                 
       maxOutputTokens: 2048,     
